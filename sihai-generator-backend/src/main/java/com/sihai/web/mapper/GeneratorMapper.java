f3a2e179478e705d3d95781aaad96f8a27c80076
Tsihai <13169186610@163.com>
2024-05-28 19:41:57 +0800

    添加 后端

---
+++ sihai-generator-backend/src/main/java/com/sihai/web/mapper/GeneratorMapper.java
@@ -0,0 +1,19 @@
+package com.sihai.web.mapper;
+
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.sihai.web.model.entity.Generator;
+
+/**
+ * @author 13169
+ * @description 针对表【generator(代码生成器)】的数据库操作Mapper
+ * @createDate 2024-05-27 01:53:34
+ * @Entity generator.domain.Generator
+ */
+public interface GeneratorMapper extends BaseMapper<Generator> {
+
+}
+
+
+
+
